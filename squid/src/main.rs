cd12688c0f83122e5a97a6de5899f48cb881fb94
DukeofStars <tristanfox009@outlook.com>
2022-07-24 16:10:25 +1000

    Migrated to use rayon

--- squid/src/main.rs
+++ squid/src/main.rs
@@ -2,6 +2,7 @@ use std::{fs, os, path::PathBuf};
 
 use clap::{Args, Parser, Subcommand};
 use compression::prelude::{DecodeExt, GZipDecoder};
+use rayon::prelude::*;
 use trauma::{download::Download, downloader::DownloaderBuilder};
 
 use directories::ProjectDirs;
@@ -91,8 +92,9 @@ async fn main() {
             println!("{} {}", "Installing".green().bold(), package.name);
             let bin_folder = data_path.join(&package.name).join("bin");
             if bin_folder.exists() && bin_folder.is_dir() {
-                for entry in fs::read_dir(bin_folder).unwrap() {
-                    let entry = entry.unwrap();
+                let vec_entries = Vec::from_iter(fs::read_dir(bin_folder).unwrap().into_iter());
+                vec_entries.par_iter().for_each(|entry| {
+                    let entry = entry.as_ref().unwrap();
                     println!(
                         "{} {}",
                         "Linking".blue().bold(),
@@ -123,27 +125,39 @@ async fn main() {
                             target.to_str().unwrap(),
                             package
                         );
-                        println!("{}", "Would you like to replace it? (y/n)");
+                        print!("{}", "Would you like to replace it? (y/n) ");
                         let mut input = String::new();
                         std::io::stdin().read_line(&mut input).unwrap();
                         if input.trim() == "y" {
                             fs::remove_file(&target).unwrap();
-                        } else {
-                            continue;
-                        }
-                    }
-                    #[cfg(windows)]
-                    {
-                        if entry.path().is_dir() {
-                            os::windows::fs::symlink_dir(entry.path(), target).unwrap();
-                        } else {
-                            os::windows::fs::symlink_file(entry.path(), target).unwrap();
-                        }
-                    }
+                            #[cfg(windows)]
+                            {
+                                if entry.path().is_dir() {
+                                    os::windows::fs::symlink_dir(entry.path(), target).unwrap();
+                                } else {
+                                    os::windows::fs::symlink_file(entry.path(), target).unwrap();
+                                }
+                            }
 
-                    #[cfg(not(windows))]
-                    os::unix::fs::symlink(entry.path(), target).unwrap();
-                }
+                            #[cfg(not(windows))]
+                            os::unix::fs::symlink(entry.path(), target).unwrap();
+                        } else {
+                            println!("{}", "Skipping...".blue().bold());
+                        }
+                    } else {
+                        #[cfg(windows)]
+                        {
+                            if entry.path().is_dir() {
+                                os::windows::fs::symlink_dir(entry.path(), target).unwrap();
+                            } else {
+                                os::windows::fs::symlink_file(entry.path(), target).unwrap();
+                            }
+                        }
+
+                        #[cfg(not(windows))]
+                        os::unix::fs::symlink(entry.path(), target).unwrap();
+                    }
+                });
             } else {
                 println!(
                     "{}: {}",
