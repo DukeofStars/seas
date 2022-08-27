daa090680597046792fec70481fedd7e1c79922e
DukeofStars <tristanfox009@outlook.com>
2022-08-27 16:16:41 +1000

    setup squid-server to use axum

--- squidserver/src/main.rs
+++ squidserver/src/main.rs
@@ -1,47 +1,5 @@
-use std::{error::Error, io, net::SocketAddr};
+use axum::*;
 
-use serde::Deserializer;
-use tokio::{
-    io::AsyncReadExt,
-    net::{TcpListener, TcpStream},
-};
-
-use squidserver::*;
-
-#[tokio::main]
-async fn main() -> Result<(), io::Error> {
-    println!("Starting server");
-
-    let listener = TcpListener::bind("127.0.0.1:5336").await?;
-
-    // connection handler
-    loop {
-        match listener.accept().await {
-            Ok((stream, addr)) => {
-                on_connect(stream, addr).await.unwrap();
-            }
-            Err(err) => return Err(err),
-        }
-    }
-}
-
-async fn on_connect(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
-    println!("New connection from {}", addr);
-
-    // Obtain header (size of the message)
-    let header: u16 = stream.read_u16_le().await?;
-    println!("Header size: {header}");
-
-    // Read contents
-    let mut buf = Vec::with_capacity(header as usize);
-    stream.read_exact(&mut buf).await?;
-    println!("Read data");
-    let string = String::from_utf8(buf.to_vec())?;
-    println!("Read utf8 string '{}'", string);
-    let msg: Message = Message::Hello(1);
-    println!("Serialised message");
-
-    println!("{} sent: {:?}", addr, msg);
-
-    Ok(())
+fn main() {
+    println!("Starting squid server");
 }
